701e505889fd683cbbd4b8b8efd7e53531d2c2ee
jakob he <jakob.he@ringcentral.com>
2021-09-18 09:29:35 +0800

    init

---
+++ src/main/java/com/jakobhe/springemqx/SpringEmqxApplication.java
@@ -0,0 +1,13 @@
+package com.jakobhe.springemqx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEmqxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringEmqxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jakobhe/springemqx/SpringEmqxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jakobhe.springemqx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEmqxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
