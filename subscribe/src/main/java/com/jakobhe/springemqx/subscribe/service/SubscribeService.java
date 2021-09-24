96006bdfb6247185ecf229223102de1fcc8e1f0f
Jakob.he <jakob.he@ringcentral.com>
2021-09-24 14:26:21 +0800

    fix issue

--- subscribe/src/main/java/com/jakobhe/springemqx/subscribe/service/SubscribeService.java
+++ subscribe/src/main/java/com/jakobhe/springemqx/subscribe/service/SubscribeService.java
@@ -1,6 +1,8 @@
 package com.jakobhe.springemqx.subscribe.service;
 
+import com.jakobhe.springemqx.subscribe.common.Context;
 import org.springframework.context.annotation.Bean;
+import org.springframework.integration.annotation.ServiceActivator;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageHandler;
 import org.springframework.messaging.MessagingException;
@@ -10,6 +12,7 @@ import org.springframework.stereotype.Service;
 public class SubscribeService {
 
     @Bean
+    @ServiceActivator(inputChannel = Context.MQTT_SUBSCRIBE_CHANNEL)
     public MessageHandler messageHandler() {
         return message -> {
             System.out.println("===================");
